1f0914f420cb5fdd95b67c27bcbcba54722cbc1b
jvlsg <jvlsg@users.noreply.github.com>
2020-04-17 11:07:06 -0300

    ADD: List of users in Index, basic profiles

--- src/social/views.py
+++ src/social/views.py
@@ -3,17 +3,17 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.template import loader
 from django.views import generic
-#from .models import User
+from .models import User
 
 # # Create your views here.
-# class IndexView(generic.ListView):
-#     template_name = 'social/index.html'
-#     context_object_name = 'latest_question_list'
+class IndexView(generic.ListView):
+    template_name = 'social/index.html'
+    context_object_name = 'users_alphabetical_list'
 
-#     def get_queryset(self):
-#         """Return the last five published questions."""
-#         return User.objects.order_by('last_name')[:5]
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return User.objects.order_by('username')[:5]
 
-def temp_index(request):
-    response = "You're looking at the index."
-    return HttpResponse(response)
+def user_profile(request,username):
+    retrieved_user = get_object_or_404(User, username=username)
+    return render(request, 'social/profile.html', {'user': retrieved_user})
